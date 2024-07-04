dc7392d66ca7b5e49a17fa9706902ac6c7864b9d
cheny <1316967697@qq.com>
2024-07-04 15:39:14 +0800

    test

--- src/components/AiPptDoc/AiPptDoc.tsx
+++ src/components/AiPptDoc/AiPptDoc.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-empty-pattern */
 
 import { genAiPptDocSrc } from "./aiPptDocSrc";
 
 interface AiPptDocProps {
   globalData: Record<string, any>;
   setGlobalData: (newValue: any) => void;
+  value?: string;
 }
 
 const iframeInlineStyle: React.CSSProperties = {
