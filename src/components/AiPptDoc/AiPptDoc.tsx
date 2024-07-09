088d19025a1b72eea37d450cff2f2e0448d3ca8e
cheny <1316967697@qq.com>
2024-07-09 10:47:40 +0800

    test

--- src/components/AiPptDoc/AiPptDoc.tsx
+++ src/components/AiPptDoc/AiPptDoc.tsx
@@ -1,29 +1,6 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// /* eslint-disable no-empty-pattern */
-
-// import { genAiPptDocSrc } from "./aiPptDocSrc";
-
-// interface AiPptDocProps {
-//   globalData: Record<string, any>;
-//   setGlobalData: (newValue: any) => void;
-//   // value?: string;
-// }
-
-// const iframeInlineStyle: React.CSSProperties = {
-//   border: "0px solid",
-//   height: "100%",
-//   width: "100%",
-// };
 
 export function AiPptDoc() {
   return (
-    // <iframe
-    //   style={iframeInlineStyle}
-    //   srcDoc={genAiPptDocSrc({
-    //     ak: globalData.ak,
-    //     sk: globalData.sk
-    //   })}
-    // />
     <div>
       hello world
     </div>
