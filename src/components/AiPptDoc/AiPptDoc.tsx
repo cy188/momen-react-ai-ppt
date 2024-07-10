bb4956623d77e6408dc08c0900d81b85ac68e041
cheny <1316967697@qq.com>
2024-07-10 16:02:17 +0800

    test

--- src/components/AiPptDoc/AiPptDoc.tsx
+++ src/components/AiPptDoc/AiPptDoc.tsx
@@ -5,12 +5,16 @@ interface AiPptDocProps {
   setGlobalData: (newValue: any) => void;
 }
 
-export function AiPptDoc({ globalData }: AiPptDocProps) {
+export function AiPptDoc({ globalData,setGlobalData }: AiPptDocProps) {
+  function handleClick() {
+    setGlobalData(111)
+  }
   return (
     <div>
       hello world
       ak: {globalData.ak}
       sk: {globalData.sk}
+      <button onClick={handleClick}>设置全局数据</button>
     </div>
   );
 }
